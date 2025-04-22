a36c29226bf3c39a0ff34ef2550fab3a7be58fe5
Danny Escalante <danny.dev@developer.dev>
2025-04-22 10:24:19 -0400

    Initial Commit

---
+++ Catholic App/Catholic_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catholic_AppApp.swift
+//  Catholic App
+//
+//  Created by Danny Escalante on 22-04-25.
+//
+
+import SwiftUI
+
+@main
+struct Catholic_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
